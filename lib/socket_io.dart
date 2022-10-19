d092e36586b2d0cd8adf251bd2073369791450ef
chappie74 <seanmsingh7@gmail.com>
2022-10-19 03:17:23 -0400

    exposed Namespace class for better typing support

--- lib/socket_io.dart
+++ lib/socket_io.dart
@@ -6,6 +6,6 @@ export 'src/engine/transport/transports.dart' show Transport, MessageHandler;
 export 'src/engine/transport/jsonp_transport.dart' show JSONPTransport;
 export 'src/engine/transport/polling_transport.dart' show PollingTransport;
 export 'src/engine/transport/websocket_transport.dart' show WebSocketTransport;
-
+export 'src/namespace.dart' show Namespace;
 export 'package:socket_io_common/src/engine/parser/parser.dart'
     show PacketParser;
